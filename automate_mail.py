5f110a8fb1608ab001b317cf02bbc3eb4c530947
Tino <tinomuzambi@gmail.com>
2019-08-06 11:28:30 +0200

    Reformatting.

--- automate_mail.py
+++ automate_mail.py
@@ -1,7 +1,8 @@
 import smtplib
 import time
 
-def sendMail(from_email, from_email_password, email_subject, email_body, to_email):
+
+def send_mail(from_email, from_email_password, email_subject, email_body, to_email):
     server = smtplib.SMTP('smtp.gmail.com', '587')
     server.ehlo()
     server.starttls()
@@ -15,18 +16,20 @@ def sendMail(from_email, from_email_password, email_subject, email_body, to_emai
 
     server.quit()
 
+
 def main():
-    count = 1 # Counter for keeping track of number of emails sent.
+    count = 1  # Counter for keeping track of number of emails sent.
     while True:
         count += 1
-        from_email = '' # Email address email is being sent from.
-        from_email_password = '' # Password of email account email is being sent from.
-        email_subject = '' # Subject of the email to be sent.
-        email_body = '' # Body of the email to be sent.
-        to_email = '' # Email address email is being sent to.
-        sendMail(from_email, from_email_password, email_subject, email_body, to_email)
+        from_email = ''  # Email address email is being sent from.
+        from_email_password = ''  # Password of email account email is being sent from.
+        email_subject = ''  # Subject of the email to be sent.
+        email_body = ''  # Body of the email to be sent.
+        to_email = ''  # Email address email is being sent to.
+        send_mail(from_email, from_email_password, email_subject, email_body, to_email)
         print('Sent ' + str(count))
-        time.sleep(5) # Change this to change the interval between sending email.
+        time.sleep(5)  # Change this to change the interval between sending email.
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
